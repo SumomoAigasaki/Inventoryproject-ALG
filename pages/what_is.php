23e9c5955c2865d011778a9196b685a0460b5332
SumomoAigasaki <sara_osorto12@hotmail.com>
2023-05-31 10:59:03 -0600

    Solucionado el error de la pagina que es INFRAG

--- pages/what_is.php
+++ pages/what_is.php
@@ -15,14 +15,27 @@ include "../includes/constantes.php";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo  $pageName; echo nameWeb; ?></title>
+     <!-- Archivos Base para el Dashboard -->
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../public/css/all.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="../public/css/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/base/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="../public/css/base/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../public/css/base/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="../public/css/base/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../public/css/adminlte.min.css"> 
+    <link rel="stylesheet" href="../public/css/base/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../public/css/base/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../public/css/base/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="../public/css/base/summernote-bs4.min.css">
     </head>
 
 <body>
