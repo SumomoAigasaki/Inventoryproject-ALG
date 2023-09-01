ecd8dfad8d783f5c79d26d8931dc1b0ed768b435
SumomoAigasaki <sara_osorto12@hotmail.com>
2023-09-01 16:36:33 -0600

    Avance=> el archivo de insert_peripherals ya guarda y muestra notificaciones

--- pages/views/insert_peripherals.php
+++ pages/views/insert_peripherals.php
@@ -2,6 +2,29 @@
 require_once "../templates/nav.php";
 require_once "../templates/menu.php";
 ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+<script src="../../public/jquery/jquery.min.js"></script>
+<script src="../../public/js/toastr.min.js">
+    toastr.options = {
+        closeButton: true,
+        debug: true,
+        progressBar: true,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+        onclick: function() {
+            window.location.href = '<?php echo BASE_URL ?>pages/views/view_peripherals.php';
+        },
+        showDuration: '300',
+        hideDuration: '1000',
+        timeOut: '5000',
+        extendedTimeOut: '1000',
+        showEasing: 'swing',
+        hideEasing: 'linear',
+        showMethod: 'fadeIn',                      
+        hideMethod: 'fadeOut'
+    }
+</script>
 <div class="content-wrapper">
 
     <!-- Content Header (Page header) -->
@@ -73,7 +96,7 @@ require_once "../templates/menu.php";
                                     <div class="col-sm-4">
                                         <div class="form-group" style="padding-top: 20px;">
                                             <label>Nombre Periferico:</label>
-                                            <input type="text" class="form-control" name="txt_nameSft" id="txt_nameSft" maxlength="45" required>
+                                            <input type="text" class="form-control" name="txt_namePRL" id="txt_namePRL" maxlength="45" required>
                                         </div>
                                     </div>
 
@@ -82,7 +105,7 @@ require_once "../templates/menu.php";
                                         <div class="form-group">
                                             <label>Tipo De Periferico: </label>
                                             <?php $resultado = mysqli_query($conn, "CALL sp_selectComponenteType()"); ?>
-                                            <select class="form-control" id="slct_componentType" name="slct_componentType"  onchange="filtrarMarcas()">
+                                            <select class="form-control" id="slct_componentType" name="slct_componentType" onchange="filtrarMarcas()">
                                                 <?php while ($row = mysqli_fetch_array($resultado)) { ?>
                                                     <option value="<?php echo $row['CPT_idtbl_component_type']; ?>"><?php echo $row['CPT_Description']; ?></option>
                                                 <?php }
@@ -150,7 +173,7 @@ require_once "../templates/menu.php";
                                         <div class="form-group">
                                             <label>Localizacion del Componente : </label>
                                             <?php $resultado = mysqli_query($conn, "CALL sp_location_select"); ?>
-                                            <select class="form-control" id="selectLocation" name="selectLocation">
+                                            <select class="form-control" id="slctLocation" name="slctLocation">
                                                 <?php while ($row = mysqli_fetch_array($resultado)) { ?>
                                                     <option value="<?php echo $row['LCT_idTbl_Location']; ?>"><?php echo $row['LCT_Description']; ?></option>
                                                 <?php }
@@ -170,7 +193,7 @@ require_once "../templates/menu.php";
                                     <div class="col-sm-6">
                                         <div class="form-group" style="padding-top: 20px;">
                                             <label>Observaciones: </label>
-                                            <textarea type="text" class="form-control" name="txt_observation" id="txt_observation" maxlength="60"> </textarea>
+                                            <textarea type="text" class="form-control" name="txt_observation" id="txt_observation" maxlength="100"> </textarea>
                                         </div>
                                     </div>
                                 </div>
@@ -178,7 +201,7 @@ require_once "../templates/menu.php";
 
                             <div class="row justify-content-center" style="padding-bottom:20px;">
                                 <div class="col-mb-3">
-                                    <button type="submit" class="btn btn-block btn-info" id="buttonInsertSFT" name="buttonInsertSFT" onclick='return validate_data();'>Guardar</button>
+                                    <button type="submit" class="btn btn-block btn-info" id="buttonInsertPRL" name="buttonInsertPRL" onclick='return validate_data();'>Guardar</button>
                                 </div>
                             </div>
                             <div class="form-group">
@@ -200,37 +223,27 @@ require_once "../templates/menu.php";
 
 <script>
     function validate_data() {
-        let nameInput = document.getElementById('txt_nameSft');
-        let manufacturerSelect = document.getElementById('slct_manufacturerSftType');
-        let versionInput = document.getElementById('txt_versionSft');
-        let typeSftSelect = document.getElementById('slct_SftType');
-        let licenceClasifSelect = document.getElementById('slct_licenceClasification');
-        let categoriSftSelect = document.getElementById('slct_category');
+        let nameInput = document.getElementById('txt_namePRL');
+        let componentTypeSelect = document.getElementById('slct_componentType');
+        let componentDescriptionInput = document.getElementById('slct_componentDescription');
+        let locationSelect = document.getElementById('slctLocation');
 
 
         if (nameInput.value.trim() === "") {
-            toastr.warning("El <b>Nombre de Software</b> esta vacio(a).<br>Por favor Ingrese un Nombre de Software valida");
+            toastr.warning("El <b>Nombre de Periferico</b> esta vacio(a).<br>Por favor Ingrese un Nombre de Periferico valido");
             nameInput.focus();
             return false;
-        } else if (versionInput.value.trim() === "") {
-            toastr.warning("La <b>Version</b> esta vacio(a).<br>Por favor Ingrese una Version valida");
-            versionInput.focus();
+        } else if (componentTypeSelect.selectedIndex == 0) {
+            toastr.warning("El <b>Tipo de Componente</b> esta vacio(a).<br>Por favor Ingrese un Tipo de Componente valida");
+            componentTypeSelect.focus();
             return false;
-        } else if (typeSftSelect.selectedIndex === 0) {
-            toastr.warning("El <b>Tipo de Software</b> esta vacio(a).<br>Por favor Ingrese un Tipo de Software valido");
-            typeSftSelect.focus();
+        } else if (componentDescriptionInput.selectedIndex == 0) {
+            toastr.warning("La <b>Especificacion del Periferico</b> esta vacio(a).<br>Por favor Ingrese una Especificacion del Periferico valida");
+            componentDescriptionInput.focus();
             return false;
-        } else if (manufacturerSelect.selectedIndex === 0) {
-            toastr.warning('El <b>Fabricante</b> esta vacio(a).<br>Por favor Ingrese una Fabricante valida');
-            manufacturerSelect.focus();
-            return false;
-        } else if (licenceClasifSelect.selectedIndex === 0) {
-            toastr.warning("El <b>Tipo de Licencia</b> esta vacio(a).<br>Por favor Ingrese un Tipo de Licencia valido");
-            licenceClasifSelect.focus();
-            return false;
-        } else if (categoriSftSelect.selectedIndex === 0) {
-            toastr.warning("La <b>Categoria </b> esta vacio(a).<br>Por favor Ingrese una Categoria valida");
-            categoriSftSelect.focus();
+        } else if (locationSelect.selectedIndex == 0) {
+            toastr.warning("La <b>Localizacion del Periferico</b> esta vacio(a).<br>Por favor Ingrese un Localizacion del Periferico valido");
+            locationSelect.focus();
             return false;
         } else {
             // Si no hay errores, procesa los datos enviados
@@ -242,40 +255,32 @@ require_once "../templates/menu.php";
 
         }
     }
-
 </script>
 
 <?php
 if (isset($_POST["buttonInsertPRL"])) {
-    
-    $nameSft = $_POST['txt_nameSft'];
-    $manufacturerSft = $_POST['slct_manufacturerSftType'];
-    $versionSft = $_POST['txt_versionSft'];
-    $serialSft = $_POST['txt_licenciaSft'];
 
-    if (empty($serialSft)) {
-        $serialSft = NULL;
-    }
-    $typeSft = $_POST['slct_SftType'];
-    $clasificationLicenceSft = $_POST['slct_licenceClasification'];
+    $namePRL = $_POST['txt_namePRL'];
+    $componentTypePRL = $_POST['slct_componentType'];
+    $descriptionPRL = $_POST['txt_description'];
+    $componentDescriptionPRL = $_POST['slct_componentDescription'];
+    $brandPRL = $_POST['slct_brand'];
+    $locationPRL = $_POST['slctLocation'];
+    $observationsPRL = $_POST['txt_observation'];
     $todayDate = date("Y-m-d");
-    $observationsSft = $_POST['txt_observation'];
-    $category = $_POST['slct_category'];
     $user = $_SESSION["User_idTbl_User"];
     $status = '2';
 
-    # Ruta de la carpeta de destino para los archivos
-    $urlSFT = '../../resources/Software/';
 
     if ($PermisoSTF) {
         try {
             //Caso contrario Guardara
-            $stmt = $conn->prepare("CALL  sp_insertSoftware(?,?,?,?,?,?,?,?,?,?,?,?)");
+            $stmt = $conn->prepare("CALL  sp_insertPeripherals(?,?,?,?,?,?,?,?,?,?)");
 
-            // $query = "CALL sp_insertSoftware('$imageSft', '$nameSft', '$manufacturerSft', '$versionSft', '$serialSft', '$typeSft', '$clasificationLicenceSft', '$todayDate', '$observationsSft', '$category', '$user', '$status');";
+            //  $query = "CALL sp_insertPeripherals( '$todayDate', '$namePRL', '$componentTypePRL', '$descriptionPRL', '$componentDescriptionPRL', '$brandPRL', '$observationsPRL', '$status', '$locationPRL', '$user');";
             // echo $query;
             // Mandamos los parametros y los input que seran enviados al PA O SP
-            $stmt->bind_param("ssssssssssss", $imageSft, $nameSft, $manufacturerSft, $versionSft, $serialSft, $typeSft, $clasificationLicenceSft, $todayDate, $observationsSft, $category, $user, $status);
+            $stmt->bind_param("ssssssssss", $todayDate, $namePRL, $componentTypePRL, $descriptionPRL, $componentDescriptionPRL, $brandPRL, $observationsPRL, $status, $locationPRL, $user);
 
 
             // Ejecutar el procedimiento almacenado
@@ -284,26 +289,20 @@ if (isset($_POST["buttonInsertPRL"])) {
                 error_log("Error en la ejecución del procedimiento almacenado: " . $stmt->error);
             }
             // Obtener el valor de la variable de salida
-            $stmt->bind_result($answerExistsSoft);
+            $stmt->bind_result($answerExistsPRL);
             $stmt->fetch();
             $stmt->close();
             $conn->next_result();
             // se extraen los valores qu     nos devuelve el procedimiento almacenado y enviamos el error
-            if ($answerExistsSoft > 0) {
-                echo '<script > toastr.success("Los datos de <b>' . $nameSft . '</b> se Guardaron de manera exitosa.", "¡¡Enhorabuena!!"); ';
+            if ($answerExistsPRL > 0) {
+                echo '<script > toastr.success("Los datos de <b>' . $namePRL . '</b> se Guardaron de manera exitosa.", "¡¡Enhorabuena!!"); ';
                 echo 'setTimeout(function() {';
-                echo '  window.location.href = "view_software.php";';
+                echo '  window.location.href = "view_peripherals.php";';
                 echo ' }, 2000); // 2000 milisegundos = 2 segundos de retraso ';
                 echo 'document.getElementById("formInsertPRL").reset(); ';
                 echo '</script>';
 
-                if ($_FILES['imgSFT']['name'] != 'default.jpg') {
-                    move_uploaded_file($_FILES['imgSFT']['tmp_name'], $urlSFT . $_FILES['imgSFT']['name']);
-                } else if (file_exists($urlSFT . $_FILES['imgSFT']['name'])) {
-                    echo '<script > toastr.info("La imagen ya existe ' . $imageSft . '")</script>;';
-                    $uploadOk = 0; //si existe lanza un valor en 0                 
 
-                }
                 exit;
             }
         } catch (mysqli_sql_exception $e) {
@@ -311,16 +310,11 @@ if (isset($_POST["buttonInsertPRL"])) {
                 // Check which specific unique field is causing the constraint violation
                 if ($e->getCode() == 1062) {
                     // Check which specific unique field is causing the constraint violation
-                    if (strpos($e->getMessage(), 'SFT_Software_Name_UNIQUE') !== false) {
+                    if (strpos($e->getMessage(), 'PRL_Name_UNIQUE') !== false) {
                         // echo "Error: ";
-                        echo '<script > toastr.error("No se pudo guardar <br> El Nombre del Software proporcionado ya está en uso. Por favor, elige un Nombre de Software diferente.","¡¡UPS!!  Advertencia: 1");';
-                        echo 'var nameInput = document.getElementById("txt_nameSft");';
+                        echo '<script > toastr.error("No se pudo guardar <br> El Nombre del Periferico proporcionado ya está en uso. Por favor, elige un Nombre de Periferico diferente.","¡¡UPS!!  Advertencia: 1");';
+                        echo 'var nameInput = document.getElementById("txt_namePRL");';
                         echo 'nameInput.focus();';
-                        echo '</script>';
-                    } elseif (strpos($e->getMessage(), 'SFT_Serial_UNIQUE') !== false) {
-                        echo '<script > toastr.error("No se pudo guardar <br> La Licencia y/o Serial proporcionado ya está en uso. Por favor, elige una Licencia y/o Serial diferente.","¡¡UPS!!  Advertencia: 2");';
-                        echo 'var serialInput = document.getElementById("txt_licenciaSft");';
-                        echo 'serialInput.focus();';
                         echo '</script>';
                     }
                 } else {
@@ -341,7 +335,6 @@ require_once "../templates/footer.php";
 
 
 <script>
-    
     function filtrarMarcas() {
         // Obtener el valor seleccionado en el primer select
         var tipoComponenteSeleccionado = document.getElementById("slct_componentType").value;
@@ -367,7 +360,7 @@ require_once "../templates/footer.php";
         if (document.querySelectorAll("#slct_componentDescription option[style='display: none;']").length === opcionesDescriptions.length - 1) {
             document.getElementById("slct_componentDescription").innerHTML = "<option value=''>No hay modelos disponibles para este fabricante</option>";
         }
-        
+
 
     }
     $(function() {
