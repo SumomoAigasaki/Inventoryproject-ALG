de83a540ac4f696d95a81af853acda8f8fa7ba8e
SumomoAigasaki <sara_osorto12@hotmail.com>
2024-01-20 17:15:20 -0600

    avance=> agrege lista de tipos de garantia

--- pages/views/dashboard_warranty.php
+++ pages/views/dashboard_warranty.php
@@ -36,19 +36,8 @@ require_once "../templates/menu.php";
                 </div>
 
                 <div class="col-sm-4  text-center ">
-
-                        <button type="button" id="downloadImages" class="btn btn-outline-dark ReportPDf" > <i class="fa fa-file-pdf"></i> PDF</a>
-                        </button>
-                      
-
-                    <!-- <div class="btn-group">
-                        <button type="button" class="btn btn-outline-dark dropdown-toggle " data-toggle="dropdown"> <ion-icon name="download-outline"></ion-icon>
-                        </button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#"><i class="fas fa-file-csv"></i> CSV</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-file-excel"></i> XLSX</a>
-                        </div>
-                    </div> -->
+                    <button type="button" id="downloadImages" class="btn btn-outline-dark ReportPDf"> <i class="fa fa-file-pdf"></i> PDF</a>
+                    </button>
                 </div>
 
                 <div class="col-sm-4">
@@ -217,7 +206,9 @@ require_once "../templates/menu.php";
             var enlacePDF = "../views/warrantyDashboardPDF.php?year=" + yearSeleccionado;
 
             // Redirigir a la página de PDF con el año en la URL
-            window.location.href = enlacePDF;
+            // window.location.href = enlacePDF;
+            // Abrir el PDF en una nueva ventana
+            window.open(enlacePDF, '_blank');
         });
         //filtro para enviar los años al RQ
         $(".filtro-year").click(function() {
@@ -243,8 +234,6 @@ require_once "../templates/menu.php";
 
         });
     });
-
-
 </script>
 
 <!-- Modal de lista de nuevos registros -->
@@ -478,11 +467,16 @@ require_once "../templates/menu.php";
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Reporte de Distribución de Registros por Tipo de Garantía (<span id="dataCountPie">datos</span>) </h4>
+
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
+                <div class="modal-footer justify-content-between">
+                    <a data-toggle="modal" data-target="#modal-xl-informacion" style="color: #3494F9;">Leer Información de Tipos de Garantía</a>
+                </div>
+
                 <table class="table">
                     <thead>
                         <tr>
@@ -513,6 +507,43 @@ require_once "../templates/menu.php";
 </div>
 
 
+<!-- Modal de Descripcion de Tipos de garantia-->
+<div class="modal fade" id="modal-xl-informacion">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Información de los Tipos de Garantía </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Este espacio ha sido creado con el propósito de brindar explicaciones más detalladas sobre el alcance y cobertura de cada una de las garantías. Nuestra intención es proporcionar una comprensión exhaustiva de los beneficios y protecciones que ofrecen, asegurando así una experiencia informada y transparente para nuestros colaboradores.entre ellas tenemos:</p>
+                <p><b>1. No Aplica:</b> Esta categoría de garantía indica que el dispositivo no cuenta con ninguna garantía vigente. En consecuencia, se considera un dispositivo sin cobertura.</p>
+                <p><b>2.- Lenovo- Servicio de transporte o de entrega: </b> Cobertura de las piezas y de la mano de obra de reparación en la que el cliente es responsable del envío (incluido el embalaje) o la entrega a un proveedor de garantía o centro de reparación autorizado.</p>
+                <p><b>3.- Recogida y devolución o recogida por mensajería: </b> Cobertura de las piezas y de la mano de obra de reparación en la que la mano de obra se proporciona in situ en su lugar de trabajo. Es posible que tenga que llevar el producto a un punto de envío, como por ejemplo una oficina de correos. El servicio de atención le informará de lo que debe hacer.</p>
+                <p><b>4.- Lenovo- Servicio in situ: </b> Cobertura de las piezas y de la mano de obra de reparación en la que la mano de obra se proporciona in situ en su lugar de trabajo. <br> • Si Lenovo determina que el defecto o el problema del producto está cubierto por la garantía de producto y no se puede resolver por teléfono, se enviará a un técnico, normalmente al siguiente día laborable. <br>La ampliación de garantía in situ + instalación técnica de CRU (Piezas que el cliente puede sustituir) amplía su cobertura para incluir la instalación in situ por parte de un técnico de las piezas CRU.</p>
+                <p><b>5.- Lenovo- Servicio de garantia internacional: </b> Cobertura de reparación de equipos diseñada para aquellos clientes que necesitan una reparación en garantía crítica durante sus viajes internacionales www.lenovo.com/internationalwarranty</p>
+                <p><b>Nota:</b> Para obtener más información sobre la garantía de Lenovo, te invitamos a <a href="https://static.lenovo.com/shop/emea/content/pdf/services-warranty/personal/Warranty-Services_CB_EMEA_es.pdf" target="_blank">visitar la página oficial</a>.</p>
+                <p><b>6.- Garantia Dell Basica: </b>Este nivel de servicio ofrece cobertura de soporte telefónico en horario laboral ante problemas de hardware, excluyendo fines de semana y festivos. El contatco 24x7x365 se da solamente via Internet mediante chat y correo electrónico. <br> Para obtener más información sobre esta garantía, te invitamos a leer <a href="https://i.dell.com/sites/csdocuments/Legal_Docs/en/us/basic-hardware-service-commercial-sd-en.pdf" target="_blank">PDF de la pagina oficial</a></p>
+                <p><b>7.- Garantia Dell ProSupport</b> Un solo punto de contacto con acceso a expertos altamente cualificados las 24 horas del dia, los 7 dias de la semana, festivos incluidos. La cobertura, además de hardware, se da también sobre aplicaciones y sistemas operativos OEM de Dell. Tambien contempla: Desplazamiento de un técnico in situ o el envio de piezas de repuesto al domicilio comercial del cliente (dependiendo del tiempo de respuesta contratado) para realizar las reparaciones necesarias. En el caso de problemas comunes, este nivel de servicio da asistencia remota con la conexión a su equipo por parte de un técnico de Dell. Asistencia de configuración para la conexión a una red simple.
+                    Acceso a foros de asistencia técnica en línea 24×7.<br> Para obtener más información sobre esta garantía, te invitamos a leer<a href="https://www.dell.com/es-es/dt/services/support-services/prosupport-client-suite.htm" target="_blank"> PDF de la pagina oficial</a></p>
+                <p><b>8.-Garantia Dell Prosupport Plus</b> ProSupport Plus es asistencia a nivel empresarial diseñada para mejorar de manera dinámica el rendimiento y la estabilidad de sus sistemas críticos por medio de inteligencia ambiental y de la experiencia adecuada para su organización.
+                    Se ha diseñado no solo para conseguir una recuperación más rápida de los problemas, sino también para ayudarle a adelantarse a ellos antes de que sucedan. Tendrá la libertad de adoptar tecnologías complejas con confianza, sabiendo que los mejores recursos de Dell estarán con usted en cada paso del camino. <br> Para saber mas sobre este servicio te invitamos a leer. <a href="https://i.dell.com/sites/csdocuments/Legal_Docs/en/us/dell-prosupport-plus-for-pcs-and-tablets-sd-en-amer.pdf" target="_blank"> PDF de la pagina de DELL</a></p>
+                <p><b>Nota:</b> para un mejor entendimiento de las garantias se proporciona cuadro comparativo entre las 3 antes mencionadas <a href="https://iberiza.es/resources/pc-support-services-1.png" target="_blank">Ver imagen</a></p>
+                <p><b>9.- Garantia HP Limitada </b> <br>
+                    <b>10.- Periodo HP de Garantia Limitada</b>El periodo de Garantía Limitada para este producto de hardware de HP es de 3 años para piezas, 3 años de mano de obra, 3 años de servicio a domicilio. El periodo de Garantía Limitada comienza en la fecha de la compra o el arrendamiento a HP, o a partir de la fecha en que HP realiza la instalación El recibo de la compra o el albarán de entrega, que muestra la fecha de compra o arrendamiento del producto. <br> Para un mejor entendimiento te invitamos a leer <a href="http://h10032.www1.hp.com/ctg/Manual/c00729261" target="_blank">PDF HP </a>
+                </p>
+                <p><b>11.- Apple- Garantia Limitada de un (1) Año</b> Apple garantiza el producto de hardware y los accesorios marca Apple que están dentro del empaque original (“Producto Apple”) contra defectos de materiales y de mano de obra, cuando se usan habitualmente de acuerdo con las pautas publicadas de Apple, durante un período de UN (1) AÑO desde la fecha de compra original por el comprador usuario final (“Período de garantía”). <br>Te invitamos a leer los terminos de Garantía limitada de un (1) año, en la <a href="https://www.apple.com/legal/warranty/products/warranty-alac-spanish.html" target="_blank"> pagina oficial de APPLE</a> </p>
+            </div>
+
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+    <!-- /.modal-content -->
+</div>
 
 
 
